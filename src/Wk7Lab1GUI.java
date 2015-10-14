ffda6681cd510f6b68bfd7c000d8025deec19b6f
SAC-CS112-Pham-Quyen <quyen714@gmail.com>
2015-10-14 09:43:25 -0700

    WK7 LAB1 GUI

---
+++ src/Wk7Lab1GUI.java
@@ -0,0 +1,24 @@
+
+
+import javax.swing.JOptionPane;
+
+
+public class Wk7Lab1GUI
+{
+
+	public static void main(String[] args)
+	{
+		
+		
+		String name =
+				JOptionPane.showInputDialog( "What is your name?" );
+
+		String message =
+				String.format( "Welcome, %s, to Java Programming!", name );
+		
+		JOptionPane.showMessageDialog( null, message );
+		
+		
+	}
+
+}
